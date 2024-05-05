b35ef7d54ee3556bfba08de8a0b92c91439d9ce3
Vignesha Shanmugam <svignesha18@gmail.com>
2024-05-05 20:23:22 +0530

    new mini project

---
+++ readme.md
@@ -0,0 +1,1 @@
+### My Mini Dictonary Project of fetching API using promise(.then and .catch)###
---
+++ script.js
@@ -0,0 +1,75 @@
+/*collectinf all the data to js*/
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+let inpWord = document.getElementById("inp-word");
+
+/*Writing a function to fetch a api and show in the display */
+btn.addEventListener("click", () => {
+    let inpWords = inpWord.value;
+    fetch(`${url}${inpWords}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWords}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+/*it is an keypress event triggered when user presses the enter button*/
+inpWord.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      return responses();
+    }
+  });
+function responses(){
+    let inpWords = inpWord.value;
+    fetch(`${url}${inpWords}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWords}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+}
+function playSound() {
+    sound.play();
+}
